0cda8722e41ff890a01a34ae4da9077268ba0ef1
Assert1veUser <tony.markin2017@yandex.ru>
2024-09-20 09:07:15 -0400

    Changes to be committed: 	new file:   .dockerignore 	new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   dockerPr1.sln 	new file:   dockerPr1/Dockerfile 	new file:   dockerPr1/Program.cs 	new file:   dockerPr1/bin/Debug/net6.0/dockerPr1 	new file:   dockerPr1/bin/Debug/net6.0/dockerPr1.deps.json 	new file:   dockerPr1/bin/Debug/net6.0/dockerPr1.dll 	new file:   dockerPr1/bin/Debug/net6.0/dockerPr1.pdb 	new file:   dockerPr1/bin/Debug/net6.0/dockerPr1.runtimeconfig.json 	new file:   dockerPr1/docker 	new file:   dockerPr1/dockerPr1.csproj 	new file:   dockerPr1/obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs 	new file:   dockerPr1/obj/Debug/net6.0/apphost 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.AssemblyInfo.cs 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.AssemblyInfoInputs.cache 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.GlobalUsings.g.cs 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.assets.cache 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.csproj.AssemblyReference.cache 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.csproj.CoreCompileInputs.cache 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.csproj.FileListAbsolute.txt 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.dll 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.genruntimeconfig.cache 	new file:   dockerPr1/obj/Debug/net6.0/dockerPr1.pdb 	new file:   dockerPr1/obj/Debug/net6.0/ref/dockerPr1.dll 	new file:   dockerPr1/obj/Debug/net6.0/refint/dockerPr1.dll 	new file:   dockerPr1/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   dockerPr1/obj/Debug/net8.0/dockerPr1.AssemblyInfo.cs 	new file:   dockerPr1/obj/Debug/net8.0/dockerPr1.AssemblyInfoInputs.cache 	new file:   dockerPr1/obj/Debug/net8.0/dockerPr1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   dockerPr1/obj/Debug/net8.0/dockerPr1.GlobalUsings.g.cs 	new file:   dockerPr1/obj/dockerPr1.csproj.nuget.dgspec.json 	new file:   dockerPr1/obj/dockerPr1.csproj.nuget.g.props 	new file:   dockerPr1/obj/dockerPr1.csproj.nuget.g.targets 	new file:   dockerPr1/obj/project.assets.json 	new file:   dockerPr1/obj/project.nuget.cache

---
+++ dockerPr1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Пожалуйста, укажите искомое значение в качестве параметра командной строки.");
+            return;
+        }
+
+        if (!int.TryParse(args[0], out int target))
+        {
+            Console.WriteLine("Пожалуйста, укажите целое число в качестве параметра командной строки.");
+            return;
+        }
+
+        int[] array = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 1001);
+        }
+
+        Array.Sort(array);
+
+        int result = BinarySearch(array, target);
+
+        if (result != -1)
+        {
+            Console.WriteLine($"Значение {target} найдено в массиве на позиции {result}.");
+        }
+        else
+        {
+            Console.WriteLine($"Значение {target} не найдено в массиве.");
+        }
+    }
+
+    static int BinarySearch(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+            {
+                return mid;
+            }
+            else if (array[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
